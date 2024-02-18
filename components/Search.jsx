0d90bae784021df72243d8d05d719b91c3af6cab
Sidhant Mishra <mishrasidhant01@gmail.com>
2024-02-18 20:59:26 +0530

    Completed Landing page(missing fonts gilroy and storybook need to be added

---
+++ components/Search.jsx
@@ -0,0 +1,13 @@
+
+const Search = () => {
+  return (
+    <form>
+      <div className="searchdest">
+        <input type="text" required="required" name="search_dest" id="search_dest" placeholder="Enter Location" />
+        {/*<span>Enter Location</span>*/}
+      </div>
+    </form>
+  )
+}
+
+export default Search
